fcaede984333e98067110d89b946704bec1a5b51
SkylerDarkWaterr <goliveirasantos1302@gmail.com>
2025-08-25 09:06:00 -0300

    Adicionar arquivos de projeto.

---
+++ ApiAppEtec/Program.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using ApiAppEtec.Data;
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<ApiAppEtecContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ApiAppEtecContext") ?? throw new InvalidOperationException("Connection string 'ApiAppEtecContext' not found.")));
+
+// Add services to the container.
+
+
+
+
+
+
+
+builder.Services.AddCors(option =>
+{
+    option.AddPolicy("PolicyApiEtec", olifrans =>
+    {
+        olifrans.AllowAnyMethod();
+        olifrans.AllowAnyHeader();
+        olifrans.AllowAnyOrigin();
+
+    });
+}
+);
+
+
+
+builder.Services.AddControllers();
+
+
+var app = builder.Build();
+
+
+// Configure the HTTP request pipeline.
+
+app.UseCors("PolicyApiEtec");
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
